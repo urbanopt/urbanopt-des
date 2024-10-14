a0c5446754bcba0a3003d1471bec8737694ab46e
Nicholas Long <nicholas.long@nrel.gov>
2024-10-13 21:50:35 -0600

    add initial unit test

--- urbanopt_des/urbanopt_results.py
+++ urbanopt_des/urbanopt_results.py
@@ -561,7 +561,7 @@ class URBANoptResults:
             feature_json = self.get_urbanopt_default_feature_report_json(self.path / "run" / f"{self.scenario_name}" / f"{building_id}")
             # Read the JSON as dictionary to the building characteristics
             #  Use a context manager for opening files
-            self.building_characteristics[building_id] = json.load(feature_json.read_text())
+            self.building_characteristics[building_id] = json.loads(feature_json.read_text())
 
             print(f"Processing building time series results {building_id}")
             feature_report = self.get_urbanopt_default_feature_report(self.path / "run" / f"{self.scenario_name}" / f"{building_id}")
