ff29340bb84ff0b773ae53658e5cccea73e5a405
Nicholas Long <1907354+nllong@users.noreply.github.com>
2026-01-20 15:50:47 -0700

    update copyright name (#61)

--- urbanopt_des/activity_to_osa/measures/inspect_and_edit_parametric_schedules/measure.rb
+++ urbanopt_des/activity_to_osa/measures/inspect_and_edit_parametric_schedules/measure.rb
@@ -1,5 +1,5 @@
 # *******************************************************************************
-# OpenStudio(R), Copyright (c) Alliance for Sustainable Energy, LLC.
+# OpenStudio(R), Copyright (c) Alliance for Energy Innovation, LLC.
 # See also https://openstudio.net/license
 # *******************************************************************************
 
