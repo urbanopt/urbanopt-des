17587cf5cf9f00b50422ab4b8da8bab71a213b0e
Nicholas Long <nicholas.long@nrel.gov>
2026-07-10 13:02:58 -0600

    precommit

--- urbanopt_des/uo_cli_wrapper.py
+++ urbanopt_des/uo_cli_wrapper.py
@@ -82,9 +82,7 @@ class UOCliWrapper:
         workspace_root = Path(__file__).resolve().parents[2]
         translator_source = workspace_root / "geojson-modelica-translator"
         if translator_source.exists():
-            new_env["PYTHONPATH"] = (
-                f"{translator_source}:{new_env['PYTHONPATH']}" if new_env.get("PYTHONPATH") else str(translator_source)
-            )
+            new_env["PYTHONPATH"] = f"{translator_source}:{new_env['PYTHONPATH']}" if new_env.get("PYTHONPATH") else str(translator_source)
         # For REopt
         if os.name != "nt":  # noqa: SIM102
             # for some reason, this doesn't work on windows, need to test, this should not cause
